84ed3635c8444005767a104b0adaffdc4b415290
MarioMH8 <mhidalgomario@gmail.com>
2023-06-16 18:22:23 +0200

    feat: integrate first preact component

---
+++ src/components/terminal.tsx
@@ -0,0 +1,9 @@
+import './terminal.css';
+
+import type { JSX } from 'preact';
+
+const Terminal = (): JSX.Element => {
+	return <section className='terminal-wrapper' data-testid='terminal-wrapper'></section>;
+};
+
+export default Terminal;
--- src/pwa.ts
+++ src/pwa.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register';
 
 registerSW({
 	immediate: true,
-})
+});
