5b3e0355d8ed992539c1f19e292aca8c432698b5
MarioMH8 <mhidalgomario@gmail.com>
2023-06-19 14:44:01 +0200

    feat: add terminal

--- src/components/terminal.tsx
+++ src/components/terminal.tsx
@@ -2,8 +2,16 @@ import './terminal.css';
 
 import type { JSX } from 'preact';
 
+import TerminalHints from './terminal-hints';
+import TerminalPrompt from './terminal-prompt';
+
 const Terminal = (): JSX.Element => {
-	return <section className='terminal-wrapper' data-testid='terminal-wrapper'></section>;
+	return (
+		<section className='terminal-wrapper' data-testid='terminal-wrapper'>
+			<TerminalHints />
+			<TerminalPrompt />
+		</section>
+	);
 };
 
 export default Terminal;
