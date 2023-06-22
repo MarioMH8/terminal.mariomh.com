bb08b2cbf5fb4cdfeef13817cba3ebd4ea03d490
MarioMH8 <mhidalgomario@gmail.com>
2023-06-22 02:26:40 +0200

    chore: update includeAssets

--- astro.config.ts
+++ astro.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 			},
 		}),
 		AstroPWA({
-			includeAssets: ['fonts/**/*.otf', '**/*.svg', '**/*.png'],
+			includeAssets: ['fonts/**/*.otf', 'fonts/**/*.woff2', '**/*.svg', '**/*.png'],
 			registerType: 'autoUpdate',
 			workbox: {
 				globPatterns: ['**/*.{js,css,html,svg,png}'],
