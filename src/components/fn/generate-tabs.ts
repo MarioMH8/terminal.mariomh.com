ba7d2e52da470b21e14c7d1fa350850a4b3458a1
MarioMH8 <mhidalgomario@gmail.com>
2024-05-24 11:25:03 +0200

    chore: include `astro check`

--- src/components/fn/generate-tabs.ts
+++ src/components/fn/generate-tabs.ts
@@ -1,11 +1,6 @@
-/**
- * Generates html tabs
- * @param {number} num - The number of tabs
- * @returns {string} tabs - Tab string
- */
-const generateTabs = (number_ = 0): string => {
+const generateTabs = (number = 0): string => {
 	let tabs = '\u00A0\u00A0';
-	for (let index = 0; index < number_; index++) {
+	for (let index = 0; index < number; index++) {
 		tabs += '\u00A0';
 	}
 
